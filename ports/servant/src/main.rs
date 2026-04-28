23dd204b49e3f84b57d402dc2b62d90cabc12880
Madcowe <34983336+Madcowe@users.noreply.github.com>
2026-04-28 21:56:32 +0100

    chore: add missing Mozilla Public License headers to servant source files

--- ports/servant/src/main.rs
+++ ports/servant/src/main.rs
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
 use servant::ant_client::AntClientManager;
 use servant::content_resolver::ContentResolver;
 use servant::ant_protocol::AntProtocolHandler;
